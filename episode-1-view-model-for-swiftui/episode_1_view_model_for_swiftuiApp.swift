61b5c9ba40c7a13cd97cf0a4344461c7d1392470
Dawid <Zubrowski>
2024-04-07 22:52:58 +0200

    Initial Commit

---
+++ episode-1-view-model-for-swiftui/episode_1_view_model_for_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  episode_1_view_model_for_swiftuiApp.swift
+//  episode-1-view-model-for-swiftui
+//
+//  Created by Dawid on 07/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct episode_1_view_model_for_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
